c8b6e29ec2eeb1d5a3a9acde21e3aa22a96a3180
liujiani505 <64924326+liujiani505@users.noreply.github.com>
2021-12-18 21:51:35 -0500

    Commit 9: Tweets update

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -18,6 +18,12 @@ class TweetsController < ApplicationController
         end
     end
 
+    def update
+        tweet = Tweet.find(params[:id])
+        tweet.update(tweet_params)
+        render json: {tweet: tweet}
+    end
+
     private
 
     def tweet_params
